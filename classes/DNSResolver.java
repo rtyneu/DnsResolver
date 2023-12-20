474bc489200ec61e465cd548e5bfa7d278f0b55f
rtyn <github.unshaken288@passmail.net>
2023-12-19 20:33:59 -0600

    get the user prompt for what is the ip address of their desire dns server and make a simple socket connection. *even thought it won't work :) I want to push that just because show myself that I accomplish something today before bed

--- classes/DNSResolver.java
+++ classes/DNSResolver.java
@@ -1,3 +1,22 @@
+import java.net.Socket;
+import java.net.InetAddress;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.io.IOException;
+
 public class DNSResolver {
+    public static void main(String[] args) {
+        System.out.println("Enter the dns server IP: ");
+        Scanner userInput = new Scanner(System.in).useDelimiter("\\.");
+        byte[] rawDNSIPAdress = new byte[4];
+        for (int index = 0; index < 4; index++) {
+            rawDNSIPAdress[index] = (byte) userInput.nextInt();
+        }
+
+        InetAddress dNSServerIP = InetAddress.getByAddress(rawDNSIPAdress);
+        Socket connection = new Socket(InetAddress.getLocalHost(), 53, dNSServerIP, 53);
+        System.out.println(connection.isBound());
+        System.out.println(connection.isConnected());
+    }
 
 }
