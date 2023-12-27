cf097ad357185d6102ac87aecadd0ee6d7bf9a38
rtyn <github.unshaken288@passmail.net>
2023-12-26 20:14:59 -0600

    create DNSServer_test.java, for test and view the datagrams. also complete the DNSResolver to send a datagram, just send it :(

--- classes/DNSResolver.java
+++ classes/DNSResolver.java
@@ -1,22 +1,30 @@
-import java.net.Socket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Scanner;
-import java.util.Arrays;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.nio.file.attribute.UserPrincipal;
+import java.net.SocketAddress;
+import java.net.InetSocketAddress;
 import java.io.IOException;
+import java.net.DatagramPacket;
 
 public class DNSResolver {
     public static void main(String[] args) {
-        System.out.println("Enter the dns server IP: ");
-        Scanner userInput = new Scanner(System.in).useDelimiter("\\.");
-        byte[] rawDNSIPAdress = new byte[4];
-        for (int index = 0; index < 4; index++) {
-            rawDNSIPAdress[index] = (byte) userInput.nextInt();
+
+        try {
+            byte[] data = new byte[32];
+            DatagramPacket testPacket = new DatagramPacket(data, 32, InetAddress.getLocalHost(), 11111);
+            DatagramSocket UDPSocket = new DatagramSocket(12345,
+                    InetAddress.getByAddress(new byte[] { 10, 0, 0, 118 }));
+            UDPSocket.send(testPacket);
+
+            System.out.println("the packet successfully sent!");
+        } catch (UnknownHostException error) {
+            System.out.println(error);
+        } catch (SocketException error) {
+            System.out.println(error);
+        } catch (IOException error) {
+            System.out.println(error);
         }
-
-        InetAddress dNSServerIP = InetAddress.getByAddress(rawDNSIPAdress);
-        Socket connection = new Socket(InetAddress.getLocalHost(), 53, dNSServerIP, 53);
-        System.out.println(connection.isBound());
-        System.out.println(connection.isConnected());
     }
-
 }
---
+++ classes/DNSServer_test.java
@@ -0,0 +1,38 @@
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.nio.file.attribute.UserPrincipal;
+import java.net.SocketAddress;
+import java.net.InetSocketAddress;
+import java.io.IOException;
+import java.net.DatagramPacket;
+
+public class DNSServer_test {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket DNSServer = new DatagramSocket(11111,
+                    InetAddress.getByAddress(new byte[] { 10, 0, 0, 118 }));
+            byte[] data = new byte[32];
+            DatagramPacket testPacket = new DatagramPacket(data, 32,
+                    InetAddress.getByAddress(new byte[] { 10, 0, 0, 118 }), 11111);
+            System.out.println("the server is running, wait for datagram....");
+            while (true) {
+                DNSServer.receive(testPacket);
+                data = testPacket.getData();
+                System.out.println("the packet received from -->" + testPacket.getSocketAddress() + "\nprot--->"
+                        + testPacket.getPort());
+                System.out.println("the data is: " + data);
+
+            }
+
+        } catch (UnknownHostException error) {
+            System.out.println(error);
+        } catch (SocketException error) {
+            System.out.println(error);
+        } catch (IOException error) {
+            System.out.println(error);
+        }
+
+    }
+}
